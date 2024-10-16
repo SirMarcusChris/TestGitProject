c86c9d73b6ed4f35689548fa086ca9c6c167c530
admin <denis@1>
2024-10-16 20:37:45 +0700

    after

--- core/contracts.py
+++ core/contracts.py
@@ -1,16 +1,15 @@
-# USER_DATA_SCHEME = {
-#     "type": "object",
-#     "properties": {
-#         "id": {"type":"number"},
-#         "email": {"type":"string"},
-#         "first_name": {"type":"string"},
-#         "last_name": {"type":"string"},
-#         "avatar": {"type":"string"}
-#     },
-#     "required": ["id", "email", "first_name", "last_name", "avatar"]
-# }
+USER_DATA_SCHEME = {
+    "type": "object",
+    "properties": {
+        "id": {"type":"number"},
+        "email": {"type":"string"},
+        "first_name": {"type":"string"},
+        "last_name": {"type":"string"},
+        "avatar": {"type":"string"}
+    },
+    "required": ["id", "email", "first_name", "last_name", "avatar"]
+}
 
-# Отсюда начинается выполнение ДЗ
 
 RESOURCE_DATA_SCHEME = {
     "type" : "object",
--- tests/test_get_user_data.py
+++ tests/test_get_user_data.py
@@ -1,7 +1,7 @@
 import httpx
 from jsonschema import validate
-from  core.contracts import RESOURCE_DATA_SCHEME
-import re
+from  core.contracts import RESOURCE_DATA_SCHEME, USER_DATA_SCHEME
+
 
 BASE_URL = "https://reqres.in/" # Основной URL
 LIST_RESOURCE = "api/unknown"
@@ -9,35 +9,36 @@ COLOR_START = "#"
 SINGLE_RESOURCE = "api/unknown/2"
 NOT_FOUND_RESOURCE = "api/unknown/23"
 
-# LIST_USERS = "api/users?page=2" # Endpoint
-# SINGLE_USER = "api/users/2"
-# EMAIL_ENDS = "@reqres.in"
-# NOT_FOUND_USER = "api/users/23"
-# AVATAR_ENDS = "-image.jpg"
 
-# def test_list_users():
-#     response = httpx.get(BASE_URL + LIST_USERS)
-#     assert response.status_code == 200
-#     data = response.json()['data']
-#
-#     for item in data:
-#         validate(item, USER_DATA_SCHEME)
-#         assert item['email'].endswith(EMAIL_ENDS) # проверяем, что `mail заканчивается на @reqres.in
-#         assert item['avatar'].endswith(str(item['id']) + AVATAR_ENDS) # проверяем, что в конце верный аватар
-#
-# def test_single_user():
-#     response = httpx.get(BASE_URL + SINGLE_USER)
-#     assert response.status_code == 200
-#     data = response.json()['data']
-#
-#     assert data['email'].endswith(EMAIL_ENDS)  # проверяем, что `mail заканчивается на @reqres.in
-#     assert data['avatar'].endswith(str(data['id']) + AVATAR_ENDS)  # проверяем, что в конце верный аватар
-#
-# def test_user_not_found():
-#     response = httpx.get(BASE_URL + NOT_FOUND_USER)
-#     assert  response.status_code == 404
 
-#Отсюда начинается выполнение ДЗ
+LIST_USERS = "api/users?page=2" # Endpoint
+SINGLE_USER = "api/users/2"
+EMAIL_ENDS = "@reqres.in"
+NOT_FOUND_USER = "api/users/23"
+AVATAR_ENDS = "-image.jpg"
+
+
+def test_list_users():
+    response = httpx.get(BASE_URL + LIST_USERS)
+    assert response.status_code == 200
+    data = response.json()['data']
+
+    for item in data:
+        validate(item, USER_DATA_SCHEME)
+        assert item['email'].endswith(EMAIL_ENDS) # проверяем, что `mail заканчивается на @reqres.in
+        assert item['avatar'].endswith(str(item['id']) + AVATAR_ENDS) # проверяем, что в конце верный аватар
+
+def test_single_user():
+    response = httpx.get(BASE_URL + SINGLE_USER)
+    assert response.status_code == 200
+    data = response.json()['data']
+
+    assert data['email'].endswith(EMAIL_ENDS)  # проверяем, что `mail заканчивается на @reqres.in
+    assert data['avatar'].endswith(str(data['id']) + AVATAR_ENDS)  # проверяем, что в конце верный аватар
+
+def test_user_not_found():
+    response = httpx.get(BASE_URL + NOT_FOUND_USER)
+    assert  response.status_code == 404
 
 def test_list_resource():
     responce = httpx.get(BASE_URL + LIST_RESOURCE)
@@ -47,7 +48,6 @@ def test_list_resource():
     for item in data:
         validate(item, RESOURCE_DATA_SCHEME)
         assert item['color'].startswith(COLOR_START) # проверяем, что значение цвета начинается с решетки
-
 
 def test_single_resource():
     responce = httpx.get(BASE_URL + SINGLE_RESOURCE)
