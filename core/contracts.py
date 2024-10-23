2d9330cba1c83ffc4c30590131e58f024df03edc
admin <denis@1>
2024-10-23 22:16:03 +0700

    add invalid registered and login tests

--- core/contracts.py
+++ core/contracts.py
@@ -1,26 +1,26 @@
-# USER_DATA_SCHEME = {
-#     "type": "object",
-#     "properties": {
-#         "id": {"type":"number"},
-#         "email": {"type":"string"},
-#         "first_name": {"type":"string"},
-#         "last_name": {"type":"string"},
-#         "avatar": {"type":"string"}
-#     },
-#     "required": ["id", "email", "first_name", "last_name", "avatar"]
-# }
-#
-# RESOURCE_DATA_SCHEME = {
-#     "type" : "object",
-#     "properties": {
-#         "id": {"type":"number"},
-#         "name": {"type":"string"},
-#         "year": {"type":"number"},
-#         "color": {"type":"string"},
-#         "pantone_value": {"type":"string"}
-#     },
-#     "required": ["id", "name", "year", "color", "pantone_value"]
-# }
+USER_DATA_SCHEME = {
+    "type": "object",
+    "properties": {
+        "id": {"type":"number"},
+        "email": {"type":"string"},
+        "first_name": {"type":"string"},
+        "last_name": {"type":"string"},
+        "avatar": {"type":"string"}
+    },
+    "required": ["id", "email", "first_name", "last_name", "avatar"]
+}
+
+RESOURCE_DATA_SCHEME = {
+    "type" : "object",
+    "properties": {
+        "id": {"type":"number"},
+        "name": {"type":"string"},
+        "year": {"type":"number"},
+        "color": {"type":"string"},
+        "pantone_value": {"type":"string"}
+    },
+    "required": ["id", "name", "year", "color", "pantone_value"]
+}
 
 CREATED_USER_SCHEME = {
     "type": "object",
@@ -56,3 +56,4 @@ LOGIN_USER_SCHEME = {
     },
     "required": ["token"]
 }
+
--- tests/test_register_and_login.py
+++ tests/test_register_and_login.py
@@ -10,23 +10,51 @@ REGISTERED_USER = "api/register"
 LOGIN_USER = "api/login"
 json_file = open('./core/new_users_data.json')
 users_data = json.load(json_file)
-
+invalid_json_file = open('./core/users_data_without_password.json')
+invalid_users_data = json.load(invalid_json_file)
 
 @allure.suite('Проверка возможности создания пользователя методом post параметризированным тестом')
 @allure.title('Проверяем создание пользователя')
 @pytest.mark.parametrize('users_data', users_data)
 def test_successful_register(users_data):
-    response = httpx.post(BASE_URL + REGISTERED_USER, json=users_data)
+    headers = {'Content-Type' : 'application/json'}
+    with allure.step('Отправка запроса с телом на регистрацию пользователя'):
+        response = httpx.post(BASE_URL + REGISTERED_USER, json=users_data, headers=headers)
     with allure.step('Проверяем код ответа'):
         assert response.status_code == 200
     with allure.step(f'Проверяем элементы из списка по схеме'):
         validate(response.json(), REGISTERED_USER_SCHEME)
 
+
+
+
+@allure.suite('Проверка неуспешной регистрации пользователя без указания пароля')
+@allure.title('Проверяем создание пользователя без указания пароля')
+@pytest.mark.parametrize('invalid_users_data', invalid_users_data)
+def test_register_without_password(invalid_users_data):
+    with allure.step('Отправка запроса с телом на регистрацию пользователя без пароля'):
+        response = httpx.post(BASE_URL + REGISTERED_USER, json=invalid_users_data)
+    with allure.step('Проверка кода ответа'):
+        assert response.status_code == 400
+
+@allure.suite('Проверка неуспешного входа пользователя без указания пароля')
+@allure.title('Проверяем создание пользователя без указания пароля')
+@pytest.mark.parametrize('invalid_users_data', invalid_users_data)
+def test_register_without_password(invalid_users_data):
+    with allure.step('Отправка запроса с телом на регистрацию пользователя без пароля'):
+        response = httpx.post(BASE_URL + LOGIN_USER, json=invalid_users_data)
+    with allure.step('Проверка кода ответа'):
+        assert response.status_code == 400
+
+
+
+
 @allure.suite('Проверка успешного входа пользователя методом post параметризированным тестом')
 @allure.title('Проверяем создание пользователя')
 @pytest.mark.parametrize('users_data', users_data)
 def test_successful_registered(users_data):
-    response = httpx.post(BASE_URL + LOGIN_USER,json=users_data)
+    with allure.step('Отправка запроса на вход пользователя'):
+        response = httpx.post(BASE_URL + LOGIN_USER,json=users_data)
     with allure.step('Проверяем код ответа'):
         assert response.status_code == 200
     with allure.step(f'Проверяем элементы из списка по схеме'):
